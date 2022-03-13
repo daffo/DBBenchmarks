20ada60c77decceefd7b6c23b64405311d99fa33
daffo <dafffo@gmail.com>
2022-03-13 15:12:14 +0100

    init project

---
+++ src/main/java/com/daffo/DBBenchmarks/App.java
@@ -0,0 +1,13 @@
+package com.daffo.DBBenchmarks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
